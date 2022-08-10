a486c458faf6e878714bef8d81a44290b222bf7a
MAGNOLIAw <39485479+MAGNOLIAw@users.noreply.github.com>
2022-08-10 13:18:38 +0800

    Update scripts.

---
+++ OCE-NET/scripts/train_ocenet_fold3.sh
@@ -0,0 +1,21 @@
+set -ex
+python3 /home/xinyi/OCE-NET/train.py \
+--dataroot /home/data/HCP \
+--checkpoints_dir ./checkpoints3 \
+--name ocenet_fodnorm_zscore_f3 \
+--model lesion_inpaint_5loss \
+--input_nc 45 \
+--output_nc 45 \
+--init_type kaiming \
+--dataset_mode fod \
+--num_threads 0 \
+--batch_size 4 \
+--beta1 0.99 \
+--lr 0.001 \
+--n_epochs 50 \
+--print_freq 1000 \
+--save_latest_freq 20000 \
+--save_epoch_freq 10 \
+--gpu_ids 0 \
+--conv_type ocenet \
+--test_fold 3 \
