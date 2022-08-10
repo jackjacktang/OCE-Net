0815cd5dbcb6d4d3d4a6cfd54cbd23fe69ee07b6
MAGNOLIAw <39485479+MAGNOLIAw@users.noreply.github.com>
2022-08-10 13:19:41 +0800

    Update scripts.

---
+++ OCE-NET/scripts/test_ocenet_gate5dec5lossse.sh
@@ -0,0 +1,14 @@
+set -ex
+python3 /home/xinyi/OCE-NET/test.py \
+--dataroot /home/data/HCP \
+--checkpoints_dir ./checkpoints3 \
+--name HCP99_fodnorm_gate5dec5lossse_zscore_f3 \
+--model lesion_inpaint_5loss \
+--input_nc 45 \
+--output_nc 45 \
+--init_type kaiming \
+--dataset_mode fod \
+--batch_size 4 \
+--gpu_ids 1 \
+--conv_type ocenet \
+--test_fold 3 \
